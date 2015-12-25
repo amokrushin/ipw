5eaff7320663fff1249458cb8364f7f149a8d3c7
amokrushin <anton@mokr.org>
2015-12-25 03:24:36 +0300

    app and controller skeleton

---
+++ app.js
@@ -0,0 +1,40 @@
+var async = require( 'async' ),
+    logger = require( './lib/logger' ),
+    rmq = require( 'am-rmq' )( logger ),
+    controllers = require( './lib/controllers' ),
+    app = {};
+
+app.startedAt = Date.now();
+
+function waitSettings( callback ) {
+    logger.info( 'Waiting for settings...' );
+    process.on( 'message', function( data ) {
+        if( data.msg === 'settings' )
+        {
+            logger.info( 'Settings were received' );
+            callback( null, data.content );
+        }
+    } );
+}
+
+function onExit() {
+    logger.info( 'SIGINT' );
+}
+
+waitSettings( function( err, settings ) {
+    app.settings = settings;
+    rmq.connect( settings.rmq );
+} );
+
+rmq.onBroadcast( 'ipw.discovery', controllers.discovery );
+rmq.onQueue( 'ipw.image-processing', {ack: true}, controllers.imageProcessing );
+
+process.on( 'SIGINT', function() {
+    onExit();
+    setTimeout( function() {
+        logger.info( 'EXIT' );
+        process.exit();
+    }, 300 );
+} );
+
+module.exports = app;
